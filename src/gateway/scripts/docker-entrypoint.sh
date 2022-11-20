e9dc58311e80fae0d63c27c440f9059af5d52d4d
AksAman@personal_pc <krsingh.aman@gmail.com>
2022-11-21 03:26:21 +0530

    remove comments

--- src/gateway/scripts/docker-entrypoint.sh
+++ src/gateway/scripts/docker-entrypoint.sh
@@ -4,9 +4,6 @@ if [ "$RABBITMQ_ENABLED" = "True" ]
 then
     echo "Waiting for rabbitmq..."
 
-    # while !</dev/tcp/$POSTGRES_HOST/$POSTGRES_PORT; do 
-    #   sleep 1; 
-    # done
     while ! nc -z $RABBITMQ_HOST $RABBITMQ_PORT; do
       sleep 0.1
     done
