8e6c20cdd2572cd16540173a1a7bbe9fc800dbf7
AksAman@personal_pc <krsingh.aman@gmail.com>
2022-11-23 17:03:47 +0530

    add registration login in e2e

--- controller/e2e_test.py
+++ controller/e2e_test.py
@@ -4,17 +4,46 @@ import base64
 
 username = "videotomp3_user"
 password = "videotomp3_password"
+email = "videotomp321@gmail.com"
 basicAuth = base64.b64encode(f"{username}:{password}".encode("utf-8")).decode("utf-8")
 
 API_GATEWAY_ENDPOINT = "http://video-to-mp3.aksaman.com"
+AUTH_ENDPOINT = "http://auth.video-to-mp3.aksaman.com"
 
 LOGIN_ROUTE = "/auth/api/v1/login"
+REGISTER_ROUTE = "/auth/api/v1/register"
+
 UPLOAD_ROUTE = "/files/api/v1/upload"
 DOWNLOAD_ROUTE = "/files/api/v1/download"
 
 
+def register_user() -> bool:
+    response = requests.post(
+        url=f"{AUTH_ENDPOINT}{REGISTER_ROUTE}",
+        headers={
+            "Content-Type": "application/json",
+        },
+        json={
+            "email": email,
+            "first_name": "videotomp3",
+            "last_name": "videotomp3user",
+            "username": username,
+            "password": password,
+            "is_admin": True,
+        },
+    )
+
+    if not response.ok:
+        print("Registration failed")
+        print(response.text, response.status_code)
+        return False
+
+    print("Registration successful")
+    return True
+
+
 # STEP: 1
-def get_token():
+def get_token(retry=False):
     # step 1: login and get token
     response = requests.post(
         f"{API_GATEWAY_ENDPOINT}{LOGIN_ROUTE}",
@@ -25,8 +54,14 @@ def get_token():
     )
 
     if not response.ok:
+        if response.status_code == 404 and not retry:
+            print(f"User not found. Registering user {username}")
+            success = register_user()
+            if success:
+                return get_token(retry=True)
+
         print("Login failed")
-        print(response.text)
+        print(response.text, response.status_code)
         return None
 
     token = response.json()["access"]
